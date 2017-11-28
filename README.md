6f6e3190261e4f2feceb9ac0187aaf633075005d
Dmitriy Karmalita <kard@ciklum.com>
2017-11-28 23:32:41 +0200

    [Initial]

---
+++ README.md
@@ -0,0 +1,71 @@
+# Esfix
+
+**Table of Content**
+
+<!-- MarkdownTOC -->
+
+- [Installation](#installation)
+- [Configuration exaple](#configuration-exaple)
+- [Command line options](#command-line-options)
+- [Troubleshooting](#troubleshooting)
+  - [Different indent sizes](#different-indent-sizes)
+
+<!-- /MarkdownTOC -->
+
+## Installation
+
+`npm i --dev @kard/esfix`
+
+Or with yarn
+
+`yarn add --dev @kard/esfix`
+
+## Configuration exaple
+
+In the `package.json`:
+
+```
+  "scripts": {
+    "esfix": "esfix"
+  },
+  "eslintConfig": {
+    "files": [ "src/**/*.js", "src/**/*.jsx" ],
+    "fix": true
+  }
+```
+
+Notes:
+* The `eslintConfig` section can be used to rewrite all of the eslint options. Please refer to the [Configuration File Formats](https://eslint.org/docs/user-guide/configuring#configuration-file-formats) and [Rules](https://eslint.org/docs/rules/) pages of eslint documentation.
+
+## Command line options
+
+`--nolocals` - Ignore any local configurations of the analyzed projects. Whet this key is presents, the only internal configuration of the package is using.
+
+`--dryrun` - No fixes are carried out linting is performed and all of its messages are shown. It's like to run eslint without `--fix`.
+
+`--showconfig` - Show summarized config. No checks or fixes are carried out in this case.
+
+## Troubleshooting
+
+### Different indent sizes
+
+It is especially painful when only indent is rewritten in a separate file (for instance: the main configuration is inside `.eslintrc.json` while rewrite is placed inside `package.json`)
+
+**Error pattern**
+```
+error: Expected indentation of ... space characters but found ... (react/jsx-indent-props) at ...
+
+...
+
+11 errors and 1 warning found.
+11 errors potentially fixable with the `--fix` option.
+```
+
+**Solution**: make sure that these rules are in sync.
+```
+  "rules": {
+    "indent": ["error", 2],
+    "react/jsx-indent": [ "error", 2 ],
+    "react/jsx-indent-props": [ "error", 2 ]
+  }
+```
---
+++ bin/esfix.js
@@ -0,0 +1,142 @@
+#!/usr/bin/env node
+/**
+ * @fileoverview Eslint CLI default configuration and fix utility.
+ * @author Dmitriy Karmalita (dmitriy.karmalita@gmail.com)
+ *
+ * Note: eslint stacks is required.
+ */
+/* eslint no-console: 0 */
+'use strict'
+
+const fs = require('fs')
+const colors = require('colors/safe')
+const { merge, extractOption, argExists } = require('../lib/utils')
+
+/**
+ * Put a config to console
+ *
+ * @param  {object} eslintConfig - a configuration to print
+ * @return {void}
+ */
+const showConfig = (eslintConfig) => {
+
+    console.log('*** CONFIG START ***')
+    console.log(eslintConfig)
+    console.log('*** CONFIG END ***')
+
+}
+
+/**
+ * Updates options with a set from file.
+ *
+ * @param  {object}     options     - configuration object to update
+ * @param  {'string'}   filePath    - path to file (js or json)
+ * @param  {'string'}   section     - name of section to use ('eslintConfig' for `package.json`)
+ * @return {object}                 - updated object
+ */
+const applyFile = (options, filePath, section) => {
+
+    if (!argExists('--nolocals') && fs.existsSync(filePath)) {
+
+        options = merge(
+            options,
+            section ? require(filePath)[section] : require(filePath)
+        )
+
+    }
+
+    return options
+
+}
+
+/**
+ * Takes an eslint formatter and its report. If there is a case of no unfixed
+ * errors, put an ok message to the console, another case outs the formatted
+ * report.
+ *
+ * @param  {function}   formatter   - a formatter returned by eslint cli
+ * @param  {object}     report      - an error report returned by eslint cli
+ * @return {void}
+ * @example
+ *      const formatter = cli.getFormatter("checkstyle")
+ *      const report = cli.executeOnFiles(...)
+ *      reportErrors(formatter,report)
+ */
+function reportErrors (formatter, report) {
+
+    if (
+        !report.errorCount &&
+        !report.warningCount &&
+        !report.fixableErrorCount &&
+        !report.fixableWarningCount) {
+
+        return colors.green('Linter-ESLint: Fix complete.')
+
+    }
+
+    return formatter(report.results)
+
+}
+
+/**
+ * Creates summarized config by applying updates to default one.
+ * @return {object}   - eslint config file
+ */
+const loadConfig = () => {
+
+    const rootPath = process.cwd()
+
+    // Take default config (standard is included)
+    let options = require('../config/eslint')
+
+    // Rewrite with options of current project's `.eslintrc.json`
+    options = applyFile(options, `${rootPath}/.eslintrc.json`)
+    // Rewrite with options of current project's `package.json`
+    options = applyFile(options, `${rootPath}/package.json`, 'eslintConfig')
+
+    return options
+
+}
+
+const CLIEngine = require('eslint').CLIEngine
+
+const eslintConfig = loadConfig()
+
+if (argExists('--showconfig')) {
+
+    showConfig(eslintConfig)
+    process.exit(0)
+
+}
+
+if (argExists('--dryrun')) eslintConfig.fix = false
+
+// Take off file patterns in separate constant
+// Note: unusual options in config cause eslint errors
+const files = extractOption(eslintConfig, 'files')
+
+const cli = new CLIEngine(eslintConfig)
+const formatter = cli.getFormatter(eslintConfig.formatter || 'codeframe')
+
+// Avaliable Formatter options:
+// ===========================
+// var formatter = cli.getFormatter();  // “stylish” (the eslint default)
+// var formatter = cli.getFormatter("checkstyle");
+// var formatter = cli.getFormatter("codeframe");  // ?
+// var formatter = cli.getFormatter("compact"); // ?
+// var formatter = cli.getFormatter("html");
+// var formatter = cli.getFormatter("jslint-xml");
+// var formatter = cli.getFormatter("json");
+// var formatter = cli.getFormatter("junit");
+// var formatter = cli.getFormatter("table");   // ?
+// var formatter = cli.getFormatter("tap");
+// var formatter = cli.getFormatter("unix"); // ? == compact
+
+// Run linting
+const report = cli.executeOnFiles(files || [ 'src/**/*.js', 'src/**/*.jsx' ])
+
+// Apply fixes if configured
+CLIEngine.outputFixes(report)
+
+// Print final message or eslint report
+console.log(reportErrors(formatter, report))
